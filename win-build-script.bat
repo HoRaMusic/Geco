023efc1b93a27026b2eaa0639344ccec93d5ea58
Alex <anadjarov@gmail.com>
2019-04-01 21:56:44 +0300

    build script fix

--- win-build-script.bat
+++ win-build-script.bat
@@ -1,33 +1,45 @@
-REM This is a stub
-
-
 REM DATE=`date +%Y-%m-%d`
-REM TODO:
-REM QTDIR=~/Qt/5.8/clang_64/
-REM echo $QTDIR
+
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+
+set QTDIR=C:\Qt\Qt5.8.0\5.8\mingw53_32\bin
+set TOOLS=C:\Qt\Qt5.8.0\Tools\mingw530_32\bin
+
+echo %QTDIR%
 
 mkdir build-win
 cd build-win
 rd full
 mkdir full
 cd full
-qmake.exe ../../geco.pro -config release
-mingw32-make.exe
-windeployqt Geco.exe
+%QTDIR%/qmake.exe ../../geco.pro -config release
+%TOOLS%/mingw32-make.exe
+REM %QTDIR%/windeployqt release/Geco.exe
 
 cd ..
 rd light
 mkdir light
 cd light
-qmake.exe ../../geco_light.pro -config release
-mingw32-make.exe
-macdeployqt.exe GecoLight.exe
+%QTDIR%/qmake.exe ../../geco_light.pro -config release
+%TOOLS%/mingw32-make.exe
+REM %QTDIR%/windeployqt.exe release/GecoLight.exe
 
 REM output:
 
-REM cd ../..
-REM cd GecoBuilds
-REM mkdir win
-REM cd win
-REM copy ../../build-osx/full/Geco ./Geco-$DATE.dmg
-REM copy ../../build-osx/light/GecoLight.dmg ./GecoLight-$DATE.dmg
+cd ../..
+cd GecoBuilds
+
+mkdir win
+cd win
+
+mkdir Geco%mydate%
+mkdir GecoLight%mydate%
+
+copy ..\..\build-win\full\release\Geco.exe Geco%mydate%\Geco.exe
+copy ..\..\build-win\light\release\GecoLight.exe GecoLight%mydate%\GecoLight.exe
+
+%QTDIR%/windeployqt.exe Geco%mydate%\Geco.exe
+%QTDIR%/windeployqt.exe GecoLight%mydate%\GecoLight.exe
+
+
+cd ../..
